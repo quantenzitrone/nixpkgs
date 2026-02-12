04e22ccef3bdb31ed0a879c2af50ce808a1dc6fb
quantenzitrone <general@dev.quantenzitrone.eu>
2026-02-12 10:24:54 +0100

    wxwidgets_3_{1,2}: rename from wxGTK3{1,2}
    fit attrname to pname

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2383,7 +2383,7 @@ with pkgs;
   };
 
   freqtweak = callPackage ../applications/audio/freqtweak {
-    wxGTK = wxGTK32;
+    wxGTK = wxwidgets_3_2;
   };
 
   frostwire-bin = callPackage ../applications/networking/p2p/frostwire/frostwire-bin.nix { };
@@ -10564,7 +10564,7 @@ with pkgs;
   diffpdf = libsForQt5.callPackage ../applications/misc/diffpdf { };
 
   diff-pdf = callPackage ../applications/misc/diff-pdf {
-    wxGTK = wxGTK32;
+    wxGTK = wxwidgets_3_2;
   };
 
   mypaint-brushes1 = callPackage ../development/libraries/mypaint-brushes/1.0.nix { };
@@ -12306,7 +12306,7 @@ with pkgs;
   };
 
   wxmaxima = callPackage ../applications/science/math/wxmaxima {
-    wxGTK = wxGTK32.override {
+    wxGTK = wxwidgets_3_2.override {
       withWebKit = true;
     };
   };
